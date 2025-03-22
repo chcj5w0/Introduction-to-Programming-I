efbf4f7813d3c2474dc1fc81ac561387dd904200
chcj5w0 <ghmpersonal@tutamail.com>
2025-03-22 18:05:41 +0900

    Week_4.c 수정

--- Week_4.c
+++ Week_4.c
@@ -1,2 +1,18 @@
 #Introduction to Programming Week 4
 
+#include <stdio.h>
+
+
+int main(void) {
+    
+    int age;
+
+    printf("What is your age?");
+
+    scanf("%d", &age);
+
+    printf("You are %d years old\n", age);
+}
+
+
+
